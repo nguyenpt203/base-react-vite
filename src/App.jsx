d70abd9778917ff24b5fb815ff52635e27945143
nguyenpt203 <nguyenpt203@gmail.com>
2024-10-12 12:06:11 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+// arrow func: () => {}
+const MyComponent = () => {
+  return (
+    <>
+      <div>NguyenPT</div>
+    </>
+  );
+};
+
 const App = () => {
   return <></>;
 };
